6cb2c9e997dfdece333174b99792d78ed472efd8
Alexandre Dias <alexandrealmeidadias@gmail.com>
2025-01-08 17:06:41 +0000

    Algumas proriedades do User entity.

--- Assembly.Projecto.Final.Domain/Models/User.cs
+++ Assembly.Projecto.Final.Domain/Models/User.cs
@@ -8,5 +8,9 @@ namespace Assembly.Projecto.Final.Domain.Models
 {
     public class User
     {
+        public string FirstName {  get; set; }
+        public string MiddleNames { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
     }
 }
