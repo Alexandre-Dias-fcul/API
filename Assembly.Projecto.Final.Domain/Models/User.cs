18b033db2c6e1cd93564c4d0b470abb83f053bb6
Alexandre Dias <alexandrealmeidadias@gmail.com>
2025-01-08 17:16:24 +0000

    Propriedade gender adicionada.

--- Assembly.Projecto.Final.Domain/Models/User.cs
+++ Assembly.Projecto.Final.Domain/Models/User.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assembly.Projecto.Final.Domain.Models
+﻿namespace Assembly.Projecto.Final.Domain.Models
 {
     public class User
     {
-        public string FirstName {  get; set; }
+        public string FirstName { get; set; }
         public string MiddleNames { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
