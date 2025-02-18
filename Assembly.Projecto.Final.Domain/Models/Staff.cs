a9838a3fb3210a4ef8857bb811c0a612e41a0a19
Alexandre Dias <alexandrealmeidadias@gmail.com>
2025-02-18 14:07:13 +0000

    Alterações no Employee e Agent

--- Assembly.Projecto.Final.Domain/Models/Staff.cs
+++ Assembly.Projecto.Final.Domain/Models/Staff.cs
@@ -16,82 +16,91 @@ namespace Assembly.Projecto.Final.Domain.Models
         {
         }
         private Staff(Name name, DateTime dateOfBirth, string gender,
-            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination, RoleType role) :
-            base(name, dateOfBirth, gender, photoFileName, isActive, hiredDate, dateOfTermination, role)
+            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination,
+             EntityLink? entityLink, int? entityLinkId) :
+            base(name, dateOfBirth, gender, photoFileName, isActive, hiredDate, dateOfTermination,
+                entityLink, entityLinkId)
         {
         }
 
         private Staff(int id, Name name, DateTime dateOfBirth, string gender,
-            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination, RoleType role) :
-            base(id, name, dateOfBirth, gender, photoFileName, isActive, hiredDate, dateOfTermination, role)
+            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination,
+             EntityLink? entityLink, int? entityLinkId) :
+            base(id, name, dateOfBirth, gender, photoFileName, isActive, hiredDate, dateOfTermination, 
+                entityLink, entityLinkId)
         {
         }
 
         private Staff(string firstName, string middleNames, string lastName, DateTime dateOfBirth, string gender,
-            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination, RoleType role) :
+            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination,
+             EntityLink? entityLink, int? entityLinkId) :
             base(firstName, middleNames, lastName, dateOfBirth, gender, photoFileName, isActive, hiredDate,
-                dateOfTermination, role)
+                dateOfTermination, entityLink, entityLinkId)
         {
 
         }
 
         private Staff(int id, string firstName, string middleNames, string lastName, DateTime dateOfBirth, string gender,
-            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination, RoleType role) :
+            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination,
+             EntityLink? entityLink, int? entityLinkId) :
             base(id, firstName, middleNames, lastName, dateOfBirth, gender, photoFileName, isActive, hiredDate,
-                dateOfTermination, role)
+                dateOfTermination, entityLink, entityLinkId)
         {
 
         }
 
         public static Staff Create(Name name, DateTime dateOfBirth, string gender, string photoFileName, bool isActive,
-           DateTime hiredDate, DateTime dateOfTermination, RoleType role)
+           DateTime hiredDate, DateTime dateOfTermination, EntityLink? entityLink, int? entityLinkId)
         {
             var staff = new Staff(name, dateOfBirth, gender, photoFileName, isActive, hiredDate, dateOfTermination
-                , role);
+                , entityLink, entityLinkId);
 
             return staff;
         }
 
         public static Staff Create(int id, Name name, DateTime dateOfBirth, string gender, string photoFileName, bool isActive
-            , DateTime hiredDate, DateTime dateOfTermination, RoleType role)
+            , DateTime hiredDate, DateTime dateOfTermination, EntityLink? entityLink, int? entityLinkId)
         {
             var staff = new Staff(id, name, dateOfBirth, gender, photoFileName, isActive, hiredDate, dateOfTermination,
-                role);
+                 entityLink, entityLinkId);
 
             return staff;
         }
 
         public static Staff Create(string firstName, string middleNames, string lastName, DateTime dateOfBirth,
             string gender, string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination,
-            RoleType role)
+             EntityLink? entityLink, int? entityLinkId)
         {
             var staff = new Staff(firstName, middleNames, lastName, dateOfBirth, gender, photoFileName, isActive,
-                hiredDate, dateOfTermination, role);
+                hiredDate, dateOfTermination, entityLink, entityLinkId);
 
             return staff;
         }
 
         public static Staff Create(int id, string firstName, string middleNames, string lastName, DateTime dateOfBirth,
-            string gender,
-           string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination, RoleType role)
+            string gender,string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination, 
+            EntityLink? entityLink, int? entityLinkId)
         {
             var staff = new Staff(id, firstName, middleNames, lastName, dateOfBirth, gender, photoFileName, isActive,
-                hiredDate, dateOfTermination, role);
+                hiredDate, dateOfTermination, entityLink, entityLinkId);
 
             return staff;
         }
 
         public void Update(Name name, DateTime dateOfBirth, string gender, string photoFileName, bool isActive,
-              DateTime hiredDate, DateTime dateOfTermination, RoleType role)
+              DateTime hiredDate, DateTime dateOfTermination, EntityLink? entityLink, 
+              int? entityLinkId)
         {
-            base.Update(name, dateOfBirth, gender, photoFileName, isActive, hiredDate, dateOfTermination, role);
+            base.Update(name, dateOfBirth, gender, photoFileName, isActive, hiredDate, dateOfTermination, 
+                entityLink, entityLinkId);
         }
 
         public void Update(string firstName, string middleNames, string lastName, DateTime dateOfBirth, string gender,
-            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination, RoleType role)
+            string photoFileName, bool isActive, DateTime hiredDate, DateTime dateOfTermination,
+            EntityLink? entityLink, int? entityLinkId)
         {
             base.Update(firstName, middleNames, lastName, dateOfBirth, gender, photoFileName, isActive, hiredDate,
-                dateOfTermination, role);
+                dateOfTermination, entityLink, entityLinkId);
         }
     }
 }
