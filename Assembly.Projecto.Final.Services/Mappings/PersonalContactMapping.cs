f3b6a1569c627b84dbf7386385e92024edc5e06f
Alexandre Dias <alexandrealmeidadias@gmail.com>
2025-03-23 12:39:06 +0000

    Elaboração de mais alguns serviços.

---
+++ Assembly.Projecto.Final.Services/Mappings/PersonalContactMapping.cs
@@ -0,0 +1,20 @@
+﻿using Assembly.Projecto.Final.Domain.Models;
+using Assembly.Projecto.Final.Services.Dtos.IServiceDtos.OtherModelsDtos;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly.Projecto.Final.Services.Mappings
+{
+    class PersonalContactMapping : Profile
+    {
+        public PersonalContactMapping()
+        {
+            CreateMap<PersonalContact, PersonalContactDto>().ReverseMap();
+            CreateMap<PersonalContact, CreatePersonalContactDto>().ReverseMap();
+        }
+    }
+}
