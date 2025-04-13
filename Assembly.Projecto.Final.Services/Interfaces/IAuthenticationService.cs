7fa6f8159b0f9bf9343a2055ecbe423d304b0b83
Alexandre Dias <alexandrealmeidadias@gmail.com>
2025-04-13 13:53:07 +0100

    Elaboração da autenticação de employee.

---
+++ Assembly.Projecto.Final.Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly.Projecto.Final.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        public string AuthenticationEmployee(string email, string senha);
+        public string AuthenticationUser(string email, string senha); 
+    }
+}
