0f2f9ef7dd6f31d5c8d1a91cc058087e3d87314e
Alexandre Dias <alexandrealmeidadias@gmail.com>
2025-09-09 15:51:17 +0100

    Criação da pasta wwwroot e configuração no program para ficheiros estáticos.

--- Assembly.Projecto.Final.WebAPI/Controllers/UserController.cs
+++ Assembly.Projecto.Final.WebAPI/Controllers/UserController.cs
@@ -11,7 +11,6 @@ namespace Assembly.Projecto.Final.WebAPI.Controllers
     public class UserController : BaseController
     {
         private readonly IUserService _userService;
-
         public UserController(IUserService userService)
         {
             _userService = userService;
--- Assembly.Projecto.Final.WebAPI/Program.cs
+++ Assembly.Projecto.Final.WebAPI/Program.cs
@@ -49,6 +49,8 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseStaticFiles();
+
 app.MapControllers();
 
 app.Run();
